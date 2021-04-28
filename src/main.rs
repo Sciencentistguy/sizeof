bbde823064ab5a4932fa11571a9b0b545bf56544
Jamie Quigley <jamie@quigley.xyz>
2021-04-28 18:50:40 +0100

    Add basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::{self, io, path::PathBuf};
+
+use structopt::StructOpt;
+
+fn main() -> Result<(), io::Error> {
+    let opt = Opt::from_args();
+    if !opt.path.is_file() {
+        if opt.path.is_dir() {
+            eprintln!("sizeof only works on files");
+        } else {
+            eprintln!(
+                "sizeof: error file not found: {}",
+                opt.path
+                    .to_str()
+                    .unwrap_or("<path contains invalid unicode so cannot be displayed>")
+            );
+        }
+        return Ok(());
+    }
+    let file = std::fs::File::open(opt.path)?;
+    let meta = file.metadata()?;
+    println!("{}", meta.len());
+    Ok(())
+}
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt()]
+    /// The path to the file.
+    path: PathBuf,
 }
